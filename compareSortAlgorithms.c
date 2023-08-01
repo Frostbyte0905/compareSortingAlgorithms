ee712b32319cb3eb9b095dc8bbb611967902ee06
Frostbyte0905 <137664731+Frostbyte0905@users.noreply.github.com>
2023-07-31 22:43:41 -0400

    Final Update compareSortAlgorithms.c

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -1,14 +1,72 @@
+// Danque, Ethan
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdbool.h>
 int extraMemoryAllocated;
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+	 if (l < r) {
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+ 
+        merge(pData, l, m, r);
+    }
 }
 
 // implement insertion sort
@@ -20,16 +78,52 @@ void insertionSort(int* pData, int n)
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 void bubbleSort(int* pData, int n)
 {
-	
+	int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (pData[j] > pData[j + 1]) {
+                swap(&pData[j], &pData[j + 1]);
+                swapped = true;
+            }
+        }
+  
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (swapped == false)
+            break;
+    }
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (pData[j] < pData[min_idx])
+            min_idx = j;
+ 
+        // Swap the found minimum element with the first element
+           if(min_idx != i)
+            swap(&pData[min_idx], &pData[i]);
+    }
 }
 
 // parses input file to an integer array
@@ -41,12 +135,31 @@ int parseData(char *inputFileName, int **ppData)
 	
 	if (inFile)
 	{
-		fscanf(inFile,"%d\n",&dataSz);
-		*ppData = (int *)malloc(sizeof(int) * dataSz);
-		// Implement parse data block
-	}
-	
-	return dataSz;
+        fscanf(inFile, "%d\n", &dataSz);
+        *ppData = (int*)malloc(sizeof(int) * dataSz);
+
+        if (*ppData)
+        {
+            for (int i = 0; i < dataSz; i++)
+            {
+                if (fscanf(inFile, "%d", &((*ppData)[i])) != 1)
+                {
+                    // Error occurred while reading data from the file
+                    free(*ppData);
+                    *ppData = NULL;
+                    dataSz = 0;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // Memory allocation failed
+            dataSz = 0;
+        }
+
+        fclose(inFile);
+    }
 }
 
 // prints first and last 100 items in the data array
